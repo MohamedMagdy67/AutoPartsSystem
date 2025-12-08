7d9abc54219ea655b7b5319a954b7ac5d91a4fa1
moham <moham@MUHAMEDD>
2025-12-08 11:37:48 +0200

    warehouse

---
+++ AutoPartsSystem/Controllers/WareHouseController.cs
@@ -0,0 +1,54 @@
+﻿using DTOsLayer.ProductDtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Model.Entities;
+using SystemContext;
+
+namespace AutoPartsSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WareHouseController : ControllerBase
+    {
+        public readonly AutoPartsSystemDB _context;
+        public WareHouseController(AutoPartsSystemDB context)
+        {
+            _context = context;
+        }
+        [Authorize]
+        [HttpGet]
+        public ActionResult GetStock() 
+        {
+            int UserID = int.Parse(User.Claims.FirstOrDefault(c => c.Type == "userID").Value);
+            var Products = _context.Products.Where(p => p.UserID == UserID).Include(PT => PT.ProductType).ToList();
+            List<WareHouseDTO> warehouse = new List<WareHouseDTO>();
+            if (Products == null) { return NotFound("No Products Exist"); }
+            foreach(var p in Products)
+            {
+                WareHouseDTO w = new WareHouseDTO()
+                { ProductName = p.Name , ProductID = p.ID , ProductTypeName = p.ProductType.Name};
+                warehouse.Add(w);
+            }
+            return Ok(warehouse);
+
+
+        }
+        [Authorize]
+        [HttpPut("{ProductID}")]
+        public ActionResult PutStock([FromRoute]int ProductID,[FromBody]int NewStock)
+        {
+            int UserID = int.Parse(User.Claims.FirstOrDefault(c => c.Type == "userID").Value);
+            Product product = _context.Products.Include(p => p.ProductType).FirstOrDefault(p => p.UserID == UserID && p.ID == ProductID);
+            if (product == null) { return NotFound("This Product Not Exist"); }
+            product.stock = NewStock;
+            _context.SaveChanges();
+            WareHouseDTO w = new WareHouseDTO() { ProductName = product.Name, ProductID = product.ID, StockOfProduct = product.stock, ProductTypeName = product.ProductType.Name };
+            return Ok(w);
+
+        }
+
+
+    }
+}
---
+++ DTOsLayer/ProductDtos/WareHouseDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOsLayer.ProductDtos
+{
+    public class WareHouseDTO
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int StockOfProduct { get; set; }
+        public string ProductTypeName { get; set; }
+
+    }
+}
